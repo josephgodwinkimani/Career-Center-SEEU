b978337cf08f403915667f274f069d530811ef7f
josephgodwinkimani <13916761+josephgodwinkimani@users.noreply.github.com>
2023-06-15 21:28:01 +0300

    chore(build): change build to static export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+/**
+* @link https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+**/
 module.exports = {
   i18n: {
     locales: ["al", "en", "mk"],
     defaultLocale: "al",
   },
+  output: 'export',
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: 'dist',
+  images: {
+    unoptimized: true
+  }
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
 };
